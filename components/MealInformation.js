1592ab04d07f836402f93a28f39e917ce759c2ef
arbiva <arbiva@post.bgu.ac.il>
2025-04-03 09:50:07 +0300

    stylig and styling components

--- components/MealInformation.js
+++ components/MealInformation.js
@@ -1,11 +1,14 @@
 import { View, Text, StyleSheet } from "react-native";
 
-function MealInformation({ duration, complexity, affordability }) {
+//the "style" in the props is not used in this component, but it is a common practice to pass it down to the parent component so that it can be used in the parent component if needed.
+//this is a common practice in react native to allow for more flexibility in styling, since we already have the styles defined in the parent component.
+//we will add it in the style array below.
+function MealInformation({ duration, complexity, affordability, style, textStyle }) {
   return (
-    <View style={styles.details}>
-      <Text style={styles.detailItem}>{duration}min</Text>
-      <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-      <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+    <View style={[styles.details , style]}>
+      <Text style={[styles.detailItem, textStyle]}>{duration}min</Text>
+      <Text style={[styles.detailItem, textStyle]}>{complexity.toUpperCase()}</Text>
+      <Text style={[styles.detailItem, textStyle]}>{affordability.toUpperCase()}</Text>
     </View>
   );
 }
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -9,18 +9,24 @@ function MealDetailScreen({ route }) {
 
   return (
     <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealInformation
         duration={selectedMeal.duration}
         affordability={selectedMeal.affordability}
         complexity={selectedMeal.complexity}
+        //the style that we import via props from "MealsInformation.js" to allow more flexability in styiling:
+        textStyle={styles.detailText} //this will set the text color to white.
       />
-      <Text>Ingedients</Text>
+      <View style={styles.subTitleContainer}>
+        <Text style={styles.subTitle}>Ingedients</Text>
+      </View>
       {selectedMeal.ingredients.map((ingredient) => (
         <Text key={ingredient}>{ingredient}</Text>
       ))}
-      <Text>Steps:</Text>
+      <View style={styles.subTitleContainer}>
+        <Text style={styles.subTitle}>Steps:</Text>
+      </View>
       {selectedMeal.steps.map((step) => (
         <Text key={step}>{step}</Text>
       ))}
@@ -29,3 +35,34 @@ function MealDetailScreen({ route }) {
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+    margin: 8,
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  subTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#e2b947",
+    textAlign: "center",
+    marginTop: 8,
+  },
+  subTitleContainer: {
+    padding: 5,
+    borderBottomColor: "#e2b947",
+    borderBottomWidth: 2,
+    marginVertical: 4,
+    marginHorizontal: 24,
+  },
+});
